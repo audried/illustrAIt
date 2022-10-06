cfb7fb4051e1d64820c530b4c740e1cf830504f0
Audrey Dockendorf <audreydockendorf1@gmail.com>
2022-10-06 12:03:17 -0400

    show top artists and tracks side by side

--- pages/api/tracks.js
+++ pages/api/tracks.js
@@ -1,14 +1,14 @@
-import {getUsersPlaylists} from '../../lib/spotify';
+import {getUsersTopTracks} from '../../lib/spotify';
 import {getSession} from 'next-auth/react';
 
-const playlist_handler = async (req, res) => {
+const track_handler = async (req, res) => {
   const {
     token: {accessToken},
   } = await getSession({req});
-  const response = await getUsersPlaylists(accessToken);
+  const response = await getUsersTopTracks(accessToken);
   const {items} = await response.json();
 
   return res.status(200).json({items});
 };
 
-export default playlist_handler;
+export default track_handler;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import {useSession, signIn, signOut} from 'next-auth/react';
 import {useState} from 'react';
-
+import homeStyles from '../styles/Home.module.css';
 
 
 export default function Home() {
   const {data: session} = useSession();
   const [artists, setArtists] = useState([]);
   const [playlists, setPlaylists] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
   const getMyPlaylists = async () => {
     const res = await fetch('/api/playlists');
@@ -20,6 +21,19 @@ export default function Home() {
     setArtists(items);
   };
 
+  const getMyTracks = async () => {
+    const res = await fetch('/api/tracks');
+    const {items} = await res.json();
+    setTracks(items);
+  };
+
+  const getAllInfo = async () => {
+    getMyArtists();
+    getMyTracks();
+  }
+
+  
+
   //TODO: instead of display top artists, change to 'generate art'
   //once art is generated, figure out somethign for users to see their stats along with the art
   if (session) {
@@ -28,23 +42,25 @@ export default function Home() {
         Signed in as {session?.token?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
         <hr />
-        <button onClick={() => getMyArtists()}>Show my top stuff</button>
-        <div>
-          {artists.length && <h1>artists:</h1>}
-          {artists.map((item) => (
-          <div key={item.id}>
-            <h5>{item.name}</h5>
+        <button onClick={() => getAllInfo() }>Show my top stuff</button>
+        <div className={homeStyles.container}>
+          <div>
+            {artists.length && <h1>artists:</h1>}
+            {artists.map((item) => (
+            <div key={item.id}>
+              <h5>{item.name}</h5>
+            </div>
+            ))}
           </div>
-          ))}
+          <div>
+            {tracks.length && <h1>tracks:</h1>}
+            {tracks.map((item) => (
+            <div key={item.id}>
+              <h5>{item.name}</h5>
+            </div>
+            ))}
+          </div>
         </div>
-        {/* <div>
-          {artists.length && <h1>artists:</h1>}
-          {artists.map((item) => (
-          <div key={item.id}>
-            <h5>{item.name}</h5>
-          </div>
-          ))}
-        </div> */}
       </>
     );
   }
