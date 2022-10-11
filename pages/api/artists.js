526bd24e15926efdb8971f80a0e3f6850cd03bd9
Audrey Dockendorf <audreydockendorf1@gmail.com>
2022-10-11 14:42:46 -0400

    fixed artist and track endpoints

--- pages/api/artists.js
+++ pages/api/artists.js
@@ -10,26 +10,13 @@ const artist_handler = async (req, res) => {
   } = await getSession({req});
 
   
-
-  let jsonArray = await Promise.all([(await getUsersTopArtists(accessToken)).json(),(await getUsersTopTracks(accessToken)).json()])
-    // .then(async (res) => {
-    //     return Promise.all(
-    //         res.map(async (data) => await data.json())
-    //     )
-    // })
-    console.log("jasonarray", jsonArray)
-  
  
-//   const [response, response2] = await Promise.all([getUsersTopArtists(accessToken),getUsersTopTracks(accessToken)])
-//   const {items2} = await response2.json();
-//   const {items} = await response.json();
+  const response = await getUsersTopArtists(accessToken)
+
+  const {items} = await response.json();
   
-//   console.log("items:",response)
-//   console.log("items2:",response2)
-
-
-//   const artistdata = setArtistData(items)
-//   return res.status(200).json(artistdata);
+  const artistdata = setArtistData(items)
+  return res.status(200).json(artistdata);
 };
 
 export default artist_handler;
