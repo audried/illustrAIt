b4c1facce6898cc71b470454c64bff1e1faf7498
Audrey Dockendorf <audreydockendorf1@gmail.com>
2022-11-10 20:30:45 -0500

    about page :)

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,14 @@
+import Header  from './components/header';
+import styles from '../styles/Home.module.css';
+import { Container, Box, Text} from '@chakra-ui/react';
+
 export default function Dashboard() {
     return(
-        <p>heyyy</p>
+        <Container maxW={'100%'} className={styles.about}>
+        <Header></Header>
+        <Box className={styles.glass} p='6' rounded='md' ml='5'>
+            <Text>heyy</Text>
+        </Box>
+        </Container>
     )
 }
