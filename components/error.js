ad275d9e17ed582497ce970b9bb3df8aad6873f4
Audrey Dockendorf <audreydockendorf1@gmail.com>
2023-02-09 11:44:16 -0800

    escape char

--- components/error.js
+++ components/error.js
@@ -5,7 +5,7 @@ export function Error(){
         <Flex direction={'column'} justify={'space-between'} align={'center'} gap={3}>
         {/* <WarningIcon w={8} h={8} color="red.500" /> */}
         <Text size={'lg'}>Something went wrong :( Please Refresh the page or close this tab and try again</Text>
-        <Text size={'lg'}>If the problem persists, send an email to audreydock1@gmail and I'll look into it ASAP</Text>
+        <Text size={'lg'}>If the problem persists, send an email to audreydock1@gmail and I&#39;ll look into it ASAP</Text>
         </Flex>
     )
 }
